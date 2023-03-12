a4aa5cdb08681cac5ed49b992afbced1af237e3a
Timur Saifiev <timerbaka@gmail.com>
2023-03-12 23:12:04 +0100

    MainActivity: Карта ключей-значений, содержащая 3D-модели

--- app/src/main/java/io/github/timerbaka/augmentedschoolbooks/MainActivity.kt
+++ app/src/main/java/io/github/timerbaka/augmentedschoolbooks/MainActivity.kt
@@ -2,8 +2,16 @@ package io.github.timerbaka.augmentedschoolbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.sceneview.ar.node.PlacementMode
 
+/**
+ * Основной класс приложения, управляющий сценой ARCore и моделями.
+ */
 class MainActivity : AppCompatActivity() {
+    // Карта ключей-значений, содержащая 3D-модели
+    private val models: Map<String, Model> = mapOf(
+        "spheres" to Model("models/spheres.glb", scaleUnits = 0.5f, placementMode = PlacementMode.INSTANT),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
